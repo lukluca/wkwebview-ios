f26d13f5cb9a8e45a829945dc12664c3ad9edefb
luca.tagliabue <luca.tagliabue@bandyer.com>
2019-08-19 22:38:24 +0200

    First Project setup

---
+++ iOSWKWebViewApp/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  iOSWKWebViewApp
+//
+//  Created by Luca Tagliabue on 19/08/2019.
+//  Copyright © 2019 Luca Tagliabue. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ iOSWKWebViewApp/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  iOSWKWebViewApp
+//
+//  Created by Luca Tagliabue on 19/08/2019.
+//  Copyright © 2019 Luca Tagliabue. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
